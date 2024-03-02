6d5c9d18cdd0b3f834a8e6095e765c14e47ce445
Jack Dawson <150480928+jacksoncdawson@users.noreply.github.com>
2024-03-02 14:43:03 -0800

    Functional for 1 guess
    - works well, but only for one guess worth of memory (big problem)
    - abstracted away some functionality to build multi-guess memory

--- main/wordle_copilot.py
+++ main/wordle_copilot.py
@@ -5,82 +5,88 @@ answers_path = "/Users/jackcdawson/Desktop/dev/Python Projects/wordle-copilot/Li
 ranks_path = "/Users/jackcdawson/Desktop/dev/Python Projects/wordle-copilot/main/ranks.json"
 words_path = "/Users/jackcdawson/Desktop/dev/Python Projects/wordle-copilot/Library/words.txt"
 
-def main(guess, state):
-  
-
-  print("check 1")
-  # check if the guess and state are valid
+def input_verify(guess, state):
   if (len(guess) != 5) or (not guess.isalpha()):
     print("Invalid guess")
     print("Guess must be 5 letters long and only contain lowercase letters")
-    return True
+    return False
   elif (len(state) != 5) or any(char not in "xyg" for char in state):
     print("Invalid state")
     print("State must be 5 letters long and only contain 'x', 'y', or 'g'")
-    return True
+    return False
+  return True
 
-  print("check 2")
-  words = helpers.read_words(words_path)
-  # answers = helpers.read_to_set(answers_path)
-  ranks = json.load(open(ranks_path))
-  
-
-  # for each letter in the guess, first check if the matching position in state is 
-  # "g"=(in the right spot), "y"=(in the word but not in the right spot), or "x"=(not in the word)
-  # if the letter is "g", remove all words that don't have that letter in that position
-  # if the letter is "y", remove all words that have that letter in that position and all words that don't have that letter in any position
-  # if the letter is "x", remove all words that have that letter in any position
-
-  print("length before: ",len(words))
+def words_narrow(words, state, guess):
   for i in range(5):
-    # correctly incluting letter in position
     if state[i] == "g":
       words = {word for word in words if word[i] == guess[i]}
     elif state[i] == "y":
       words = {word for word in words if (word[i] != guess[i] and guess[i] in word)}
     elif state[i] == "x":
       words = {word for word in words if guess[i] not in word}
-  print("length after: ",len(words))
-  print("words after: ", words)
+  return words
 
-  # for each word left, sum the ranks of the letters in the word
-  # keep track of the word with the highest rank
-  # if there are multiple words with the same highest rank, keep track of the one with the fewest letters in common with the guess (to maximize the amount of information gained) 
-  # return the word with the highest rank
+def answers_narrow(answers, words):
+  return [answer for answer in answers if answer in words]
 
-  print("check 4")
-  max_rank = 0
+def get_best_word(words, ranks, guess):
   best_word = ""
+  best_rank = 0
   for word in words:
-    print("check 4.1")
-
     rank = 0
     for i in range(5):
-      for j in range(5):
-        if i != j:
-          rank += ranks[i][j] if word[i] == guess[j] else 0
-
-    print("check 4.2")
-    if rank > max_rank:
-      max_rank = rank
+      rank += ranks[word[i]][str(i)]
+    if rank > best_rank:
+      best_rank = rank
       best_word = word
-      print(best_word)
+    elif rank == best_rank:
+      if sum([1 for i in range(5) if word[i] == guess[i]]) < sum([1 for i in range(5) if best_word[i] == guess[i]]):
+        best_word = word
+  return best_word
 
-  print("check 5")
-  print(best_word)
-  return True
+def main(guess, state):
+
+  # if input is invalid, return False
+  if not input_verify(guess, state):
+    return False
+
+  words = helpers.read_words(words_path)
+  answers = helpers.read_words(answers_path)
+  ranks = json.load(open(ranks_path))
+
+  # narrow down words based on the state
+  words = words_narrow(words, state, guess)
+  
+  if len(words) > 20:
+    print("Remaining number of words: ", len(words))
+  else:
+    print("Remaining words: ", words)
+
+  # narrow down answers based on remaining words
+  answers = answers_narrow(answers, words)
+
+  if len(answers) > 20:
+    print("Remaining number of answers: ", len(answers))
+  else:
+    print("Remaining answers: ", answers)
+
+  # find the word with the highest rank
+  best_word = get_best_word(words, ranks, guess)
+
+  return best_word
 
 if __name__ == "__main__":
   
   try:
-    main("print", "yyxxg")
-  except:
-    print('Error Occurred in main() function.')
+    next_guess = main("dowel", "xxxxx")
+  except Exception as e: 
+    print(f'Error Occurred in main() function: {e}') 
     exit(1)
+
+  if next_guess == False:
+    # input_verify() will print error message
+    exit(1)
+
+  print("Best Next guess: ", next_guess)
   
   exit(0)
-
-
-# word: sport
-# guess: print
-# state: yyxxg
