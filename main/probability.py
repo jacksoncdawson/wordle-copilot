0568427d2f858924afa6cecfba9e3f9f083b2f92
Jack Dawson <150480928+jacksoncdawson@users.noreply.github.com>
2024-03-25 16:58:34 -0700

    struggle

--- main/probability.py
+++ main/probability.py
@@ -10,41 +10,68 @@ test_words_path = "/Users/jackcdawson/Desktop/dev/Python Projects/wordle-copilot
 words = json.load(open(words_path))
 states = json.load(open(states_path))
 
+# new edit
 def words_narrow(words: list[str], state: str, guess: str) -> list[str]:
 
   letter_count = {}
+  green = {}
+  yellow = {}
 
   for i in range(5):
-
     if guess[i] not in letter_count:
       letter_count[guess[i]] = 0
 
+  # lock-in greens
+  for i in range(5):
     if state[i] == "g":
-      letter_count[guess[i]] += 1
-      words = [word for word in words if word[i] == guess[i]]
-    elif state[i] == "y":
-      letter_count[guess[i]] += 1
-      words = [word for word in words if (word[i] != guess[i]) and (guess[i] in word)] 
-    elif state[i] == "x":
-      # if the letter has not been seen in a "g" or "y" position, remove all words with that letter
-      if not any(guess[i] == guess[j] and state[j] in ["g", "y"] for j in range(5)):
-        words = [word for word in words if guess[i] not in word]
-      else:
-        # if the letter has been seen in a "g" or "y" position, remove all words with that letter in the same position
-        words = [word for word in words if guess[i] != word[i]]
 
-  # remove any words left that have less than the recorded count of each letter
+      letter_count[guess[i]] += 1
+
+      # update green dict
+      if guess[i] in green:
+        green[guess[i]].append(i)
+      else:
+        green[guess[i]] = [i] # e.g. green = {"e": [4]}
+
+      words = [word for word in words if word[i] == guess[i]]
+  
+  # lock-out yellows
+  for i in range(5):
+    if state[i] == "y":
+
+      letter_count[guess[i]] += 1
+
+      # update yellow dict
+      if guess[i] in yellow:
+        yellow[guess[i]].append(i)
+      else:
+        yellow[guess[i]] = [i] # e.g. yellow = {"s": [0]}
+
+      words = [word for word in words if (word[i] != guess[i]) and (guess[i] in word)]
+
+  # handle greys
+  for i in range(5):
+    if state[i] == "x":
+      if guess[i] in green: 
+        # remove all words with the letter in every position except the green positions
+        words = [word for word in words if all((word[j] != guess[i]) or (j in green[guess[i]]) for j in range(5))]
+      elif guess[i] in yellow: 
+        # remove all words with the letter in the current position
+        words = [word for word in words if word[i] != guess[i]]
+      else:
+        # remove all words with the letter in any position
+        words = [word for word in words if guess[i] not in word]
+
+  # remove any words left that have less than or more than the recorded count of each letter
   for letter in letter_count:
     words = [word for word in words if word.count(letter) >= letter_count[letter]]
 
   return words
 
-# given a word (e.g. "weary") and state (e.g. "xxxxx"): returns the probability of the word having that state (assuming all words are equally likely)
 def prob_of_state(word: str, state: str) -> float:
   remaining_words = words_narrow(words, state, word)
   return (len(remaining_words) / len(words))
 
-# given a word (e.g. "weary"): returns a list of tuples of the form (state, probability) sorted by probability in descending order
 def pmf_of_word(word: str) -> list[tuple]:
   state_probabilities = []
 
@@ -57,7 +84,6 @@ def pmf_of_word(word: str) -> list[tuple]:
 
   return state_probabilities_sorted
 
-# given a word (e.g. "weary"): graphs a bar chart of the probability mass distribution of the word as a function of state using plotly
 def graph_pmf(word: str) -> None:
 
   # get pmf of word as a list of tuples
@@ -80,23 +106,16 @@ def graph_pmf(word: str) -> None:
 
   return
 
-# given a word (e.g. "weary"): returns the entropy of the word
 def entropy(word: str) -> float:
 
   # get pmf of word as a list of tuples
   state_probabilities = pmf_of_word(word)
 
-  expected_info = 0
-
   # calculate entropy
-  for state, prob in state_probabilities:
-    if prob > 0:
-      information = -math.log2(prob)
-      expected_info += prob * information
+  expected_info = sum([-prob * math.log2(prob) for state, prob in state_probabilities if prob > 0])
 
   return expected_info
 
-# collects naive 1st guess entropy data for all words in the library
 def collect_entropy_data() -> None:
 
   entropy_data = {}
@@ -115,10 +134,26 @@ def collect_entropy_data() -> None:
 
   return
 
+
+# tooling
+def avg_remaining_words(word: str) -> float:
+
+  avg_remaining = 0
+  for state in states:
+    avg_remaining += len(words_narrow(words, state, word))
+  
+  return avg_remaining / 243
+
 if __name__ == "__main__":
 
-  # word = "tares"
-  # graph_pmf(word)
-  # print(word, entropy(word))
+  word = "salon"
 
-  collect_entropy_data()
+  print(entropy(word))
+
+  print(avg_remaining_words(word))
+
+  remaining = words_narrow(words, "gggxx", word)
+  print(remaining)
+
+
+  
